2288d040d2e099df3ed0b3be5eea61cecbca0aeb
Aran Leite <hyoretsu@gmail.com>
2021-01-29 10:59:40 -0300

    Fixed "Cursor" crash in check hook

--- src/init/checkHook/index.ts
+++ src/init/checkHook/index.ts
@@ -1,22 +1,26 @@
 const auraTierChecks = (): (() => void)[] => {
- const checkFuncsArray = Game.ObjectsById.filter(building => building.vanilla === 1).map(building => {
-  return () => {
-   if (Game.Has('Aura gloves') && building.level >= 5) {
-    Game.Unlock(building.tieredUpgrades.aura.name);
-   }
-  };
- });
+ const checkFuncsArray = Game.ObjectsById.filter(building => building.vanilla === 1 && building.name !== 'Cursor').map(
+  building => {
+   return () => {
+    if (Game.Has('Aura gloves') && building.level >= 5) {
+     Game.Unlock(building.tieredUpgrades.aura.name);
+    }
+   };
+  },
+ );
 
  return checkFuncsArray;
 };
 const luminousTierChecks = (): (() => void)[] => {
- const checkFuncsArray = Game.ObjectsById.filter(building => building.vanilla === 1).map(building => {
-  return () => {
-   if (Game.Has('Luminous gloves') && building.level >= 10) {
-    Game.Unlock(building.tieredUpgrades.luminous.name);
-   }
-  };
- });
+ const checkFuncsArray = Game.ObjectsById.filter(building => building.vanilla === 1 && building.name !== 'Cursor').map(
+  building => {
+   return () => {
+    if (Game.Has('Luminous gloves') && building.level >= 10) {
+     Game.Unlock(building.tieredUpgrades.luminous.name);
+    }
+   };
+  },
+ );
 
  return checkFuncsArray;
 };
